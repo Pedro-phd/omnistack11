19c9a3f2610a50a0353c4d5ef9b98df9e7ece15d
pedro-phd <pedro_dominici@outlook.com>
2020-03-28 18:29:21 -0300

    backend finalizado

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const ongController = require('./controllers/ongController')
 const incidentController = require('./controllers/incidentController')
+const profileController = require('./controllers/profileController')
+const sessionController = require('./controllers/sessionController')
 const routes = express.Router();
 
 
@@ -10,8 +12,11 @@ routes.post('/ongs', ongController.create);
 
 routes.get('/incidents', incidentController.index);
 routes.post('/incidents', incidentController.create);
-
-
 routes.delete('/incidents/:id', incidentController.delete)
+
+routes.get('/profile', profileController.index)
+
+routes.post('/sessions',sessionController.create )
+
 
 module.exports = routes;
