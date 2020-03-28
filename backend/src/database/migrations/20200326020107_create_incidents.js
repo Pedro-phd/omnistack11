01edd36e04e6c2ee53ab82e55b0df87a32f6c02d
pedro-phd <pedro_dominici@outlook.com>
2020-03-28 17:51:30 -0300

    28 de março

--- backend/src/database/migrations/20200326020107_create_incidents.js
+++ backend/src/database/migrations/20200326020107_create_incidents.js
@@ -1,6 +1,6 @@
 
 exports.up = function(knex) {
-  return knex.schema.createTable('incidentes', function(table){
+  return knex.schema.createTable('incidents', function(table){
       
       table.increments();
       table.string('title').notNullable();
